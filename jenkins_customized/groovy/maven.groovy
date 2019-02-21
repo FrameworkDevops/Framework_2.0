24bc4af5da545d105f20fde085c02df183575878
FrameworkDevops <47414740+FrameworkDevops@users.noreply.github.com>
2019-02-21 16:53:37 +0530

    Update maven.groovy

--- jenkins_customized/groovy/maven.groovy
+++ jenkins_customized/groovy/maven.groovy
@@ -1,10 +1,27 @@
-import jenkins.*
-import jenkins.model.*
-import hudson.*
-import hudson.model.*
+import hudson.tasks.Maven 
+import hudson.tasks.Maven.MavenInstallation; 
+import hudson.tools.InstallSourceProperty; 
+import hudson.tools.ToolProperty; 
+import hudson.tools.ToolPropertyDescriptor 
+import hudson.tools.ZipExtractionInstaller; 
+import hudson.util.DescribableList 
+import jenkins.model.Jenkins; 
+ 
+def extensions = Jenkins.instance.getExtensionList(Maven.DescriptorImpl.class)[0] 
+ 
+List<MavenInstallation> installations = [] 
 
-a=Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
-b=(a.installations as List);
-b.add(new hudson.tasks.Maven.MavenInstallation("MAVEN3", "/home/jenkins/apache-maven/apache-maven-3.2.3", []));
-a.installations=b
-a.save()
+mavenToool = ['name': 'maven3', 'url': 'file:/var/jenkins_home/downloads/apache-maven-3.5.2-bin.tar.gz', 'subdir': 'apache-maven-3.5.2'] 
+ 
+println("Setting up tool: ${mavenToool.name} ") 
+ 
+def describableList = new DescribableList<ToolProperty<?>, ToolPropertyDescriptor>() 
+def installer = new ZipExtractionInstaller(mavenToool.label as String, mavenToool.url as String, mavenToool.subdir as String); 
+ 
+describableList.add(new InstallSourceProperty([installer])) 
+ 
+installations.add(new MavenInstallation(mavenToool.name as String, "", describableList)) 
+ 
+extensions.setInstallations(installations.toArray(new MavenInstallation[installations.size()])) 
+extensions.save() 
+
